30a0a248e57e0d54897efd6879ef0623dcf99f7c
hamzah abu salim <hamzahofficial69@gmail.com>
2025-09-13 05:22:44 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haabu-sa <haabu-sa@amman.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 03:34:48 by haabu-sa          #+#    #+#             */
+/*   Updated: 2025/09/13 03:34:48 by haabu-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42  
+#endif
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+// char	*ft_strjoin(char *s1, char *s2);
+// char	*ft_substr(char *s, int start, int len);
+size_t		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+#endif
